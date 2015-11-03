253fdcb98fc335bd9a1900b8eb5c9c2a8b24c3a8
Pavel Komiagin <pavel.komiagin@gmail.com>
2015-11-03 12:47:19 +0300

    Add Queue

--- src/datatypes.js
+++ src/datatypes.js
@@ -44,8 +44,43 @@ var DataTypes = function() {
     };
   };
 
+  var Queue = function() {
+    var data = [];
+
+    /**
+     * Add element at the end of queue
+     * @returns {Number|undefined}
+     */
+    var enqueue = function(element) {
+      data.push(element)
+    };
+
+    /**
+     * Decrement size of queue and return the element that was at the start of queue
+     * @returns {Number|undefined}
+     */
+    var dequeue = function() {
+      return data.unshift();
+    };
+
+    /**
+     * Return the size of queue
+     * @returns {Number}
+     */
+    var size = function() {
+      return data.size;
+    };
+
+    return {
+      enqueue: enqueue,
+      dequeue: dequeue,
+      size: size
+    }
+  };
+
   return {
-    Stack: Stack
+    Stack: Stack,
+    Queue: Queue
   }
 };
 
