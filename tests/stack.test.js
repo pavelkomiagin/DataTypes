8c9d4e4ddd8216307932634849b525f03d869cc6
Pavel Komiagin <pavel.komiagin@gmail.com>
2015-11-11 21:51:51 +0300

    Add tests for Stack

---
+++ tests/stack.test.js
@@ -0,0 +1,45 @@
+var Stack = require('../src/stack');
+
+describe('Stack', function() {
+  it('push method increase size by 1', function() {
+    var stack = new Stack();
+
+    stack.push('hello');
+    assert.equal(stack.size(), 1);
+    stack.push('world');
+    assert.equal(stack.size(), 2);
+  });
+  
+  it('pop method decrease size by 1', function() {
+    var stack = new Stack();
+
+    stack.push('hello');
+    stack.push('this');
+    stack.push('world');
+    var elem = stack.pop();
+    assert.equal(stack.size(), 2);
+  });
+  
+  it('pop method return correct value', function() {
+    var stack = new Stack();
+  
+    stack.push('hello');
+    stack.push('this');
+    stack.push('world');
+    assert.equal(stack.pop(), 'world');
+    assert.equal(stack.pop(), 'this');
+    assert.equal(stack.pop(), 'hello');
+  });
+  
+  it('peek method return top element and not decrease stack size', function() {
+    var stack = new Stack();
+  
+    stack.push('hello');
+    stack.push('this');
+    stack.push('world');
+    assert.equal(stack.peek(), 'world');
+    assert.equal(stack.size(), 3);
+    assert.equal(stack.peek(), 'world');
+    assert.equal(stack.size(), 3);
+  });
+});
