d1f4939442a6df7085de9a9ab110804dd2eed1a8
Pavel Komiagin <pavel.komiagin@gmail.com>
2015-11-12 19:07:51 +0300

    Add Queue tests

---
+++ tests/queue.test.js
@@ -0,0 +1,33 @@
+var Queue = require('../src/queue');
+
+describe('Queue', function() {
+  it('enqueue method increase size by 1', function() {
+    var queue = new Queue();
+
+    queue.enqueue('hello');
+    assert.equal(queue.size(), 1);
+    stack.enqueue('world');
+    assert.equal(queue.size(), 2);
+  });
+  
+  it('dequeue method decrease size by 1', function() {
+    var queue = new Queue();
+
+    queue.enqueue('hello');
+    queue.enqueue('this');
+    queue.enqueue('world');
+    var elem = queue.dequeue();
+    assert.equal(queue.size(), 2);
+  });
+  
+  it('dequeue method return correct value', function() {
+    var queue = new Queue();
+  
+    queue.enqueue('hello');
+    queue.enqueue('this');
+    queue.enqueue('world');
+    assert.equal(queue.dequeue(), 'world');
+    assert.equal(queue.dequeue(), 'this');
+    assert.equal(queue.dequeue(), 'hello');
+  });
+});
