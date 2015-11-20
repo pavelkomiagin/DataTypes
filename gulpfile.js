24f0eff46183dea91f79c4cee08043c64377c74e
Pavel Komiagin <pavel.komiagin@gmail.com>
2015-11-20 16:32:41 +0300

    Create gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp'),
+    mocha = require('gulp-mocha')
+
+gulp.task('test', function () {
+    return gulp.src('./test/**/*.js', {read: false})
+        .pipe(mocha({ reporter: 'spec', bail: true }));
+});
+
+gulp.task('default', ['test'], function() {
+});
